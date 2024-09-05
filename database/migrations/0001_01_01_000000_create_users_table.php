da51b96b6d22a0cf3af94cb8ebe6facb486a1f78
afanokta <afanoktafianto99@gmail.com>
2024-09-05 17:38:31 +0700

    add fullname

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username');
+            $table->string('fullname');
             $table->enum('role', ['ADMIN', 'USER'])->default('USER');
             $table->enum('gender', ['MALE', 'FEMALE'])->default('MALE');
             $table->string('email')->unique();
